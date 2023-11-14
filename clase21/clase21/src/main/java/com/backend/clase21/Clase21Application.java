9def41494f82fc1cf0db110d4d6e3da930ae8575
lumurga <mlucianamurga@gmail.com>
2023-11-14 20:32:12 -0300

    Clase 21

---
+++ clase21/clase21/src/main/java/com/backend/clase21/Clase21Application.java
@@ -0,0 +1,13 @@
+package com.backend.clase21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase21Application.class, args);
+	}
+
+}
---
+++ clase21/clase21/src/test/java/com/backend/clase21/Clase21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.clase21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
