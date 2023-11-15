c58955036832354902461958b07ed1c8ac6bf3cb
lumurga <mlucianamurga@gmail.com>
2023-11-14 22:52:19 -0300

    Clase 22 - Relaciones entre entidades

---
+++ clase22/clase22/src/main/java/com/backend/clase22/Clase22Application.java
@@ -0,0 +1,13 @@
+package com.backend.clase22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase22Application.class, args);
+	}
+
+}
---
+++ clase22/clase22/src/test/java/com/backend/clase22/Clase22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.clase22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
